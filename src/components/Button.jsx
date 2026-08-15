00c0659edc3fa3451a2e3dbb8867e6c3d13d09a5
ujjwal-uc <ujjwal.kesherwani@ucertify.com>
2026-08-15 13:42:32 +0530

    work on banner section...

---
+++ src/components/Button.jsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Button = ({ btnLink, btnName }) => {
+  return (
+    <>
+      <div className="relative w-fit">
+        <Link
+          className="group relative py-4 px-7.5 bg-[#d7fb00] rounded-[50px] flex items-center w-fit lg:text-[16px] capitalize font-bold"
+          href={btnLink}
+        >
+          {btnName}
+
+          <div
+            className="
+            absolute
+            right-[-22%]
+            top-1/2
+            w-12.5
+            h-12.5
+            bg-[#d7fb00]
+            rounded-full
+            flex
+            items-center
+            justify-center
+            border-2
+            border-[#171e2e]
+            -translate-y-1/2
+            -rotate-45
+            transition-transform
+            duration-300
+            group-hover:rotate-0
+            group-hover:bg-white
+            
+            "
+          >
+            <Image
+              className="opacity-70 group-hover:opacity-100"
+              src="/arrow-black.svg"
+              width={25}
+              height={25}
+              alt="link arrow"
+            />
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Button;
--- src/views/HomePage/Banner.jsx
+++ src/views/HomePage/Banner.jsx
@@ -1,4 +1,24 @@
+import Button from "@/components/Button";
+
 const Banner = () => {
+  const DATA = [
+    {
+      _id: 1243234,
+      count: 1200,
+      title: "Active Members",
+    },
+    {
+      _id: 29078897,
+      count: 12,
+      title: "Certified Trainers",
+    },
+    {
+      _id: 39898689,
+      count: 20,
+      title: "Year Of Experience",
+    },
+  ];
+
   return (
     <>
       <div className="overflow-hidden relative flex items-center">
@@ -12,15 +32,55 @@ const Banner = () => {
 
         <div className="h-screen w-screen flex items-center bg-[#171e2e8c] z-20">
           <div className="container">
-           <div className="max-w-3/6">
-             <h2 className="lg:text-[68px] lg:leading-20 font-bold text-white uppercase mb-4">Your body can <span className="text-[#d7fb00]">stand <br/> almost</span> anything.</h2>
-            <p className="text-white lg:text-lg lg:leading-7.25">
-              It&apos;s your mind that needs convincing. Push past your limits,
-              stay committed, and watch as your body transform into powerhouse
-              of strength and resilience. Start your journey today & truly
-              capable of!
-            </p>
-           </div>
+            <div className="max-w-3/6">
+              <h2 className="lg:text-[68px] lg:leading-20 font-bold text-white uppercase mb-4">
+                Your body can{" "}
+                <span className="text-[#d7fb00]">
+                  stand <br /> almost
+                </span>{" "}
+                anything.
+              </h2>
+              <p
+                className="
+                text-white 
+                lg:text-lg 
+                lg:leading-7.25 
+                relative
+                pl-5
+                before:absolute
+                before:left-0
+                before:top-0
+                before:h-full
+                before:w-1
+                overflow-hidden
+                before:bg-[#d7fb00]
+                before:content-['']
+                mb-10"
+              >
+                It&apos;s your mind that needs convincing. Push past your
+                limits, stay committed, and watch as your body transform into
+                powerhouse of strength and resilience. Start your journey today
+                & truly capable of!
+              </p>
+
+              <ul className="text-white flex gap-14 mb-10">
+                {DATA.map(({ _id, count, title }) => {
+                  return (
+                    <>
+                      <li key={_id}>
+                        <h4 className="lg:text-[30px] lg:leading-9 font-bold mb-1">
+                          {count}+
+                        </h4>
+                        <p className="lg:text-lg lg:leading-7.25">{title}</p>
+                      </li>
+                    </>
+                  );
+                })}
+              </ul>
+
+
+              <Button btnLink='/about' btnName="get started" />
+            </div>
           </div>
         </div>
       </div>
